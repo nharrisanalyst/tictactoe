50a8ca06f5783e5be3d397b7d1888ac472712cda
Nathan <nharrisanalyst@gmail.com>
2018-01-27 14:47:55 -0800

    readme fixed

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from '../reducer/reducer';
+import thunk from 'redux-thunk';
 
 export function storeCreator(){
-      return createStore(reducer)
+      return createStore(reducer,applyMiddleware(thunk))
 
 }
